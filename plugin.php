a49de92ca9a6ce7cd2d7e0dd9bd84bbb46426d71
jeph864 <jephte.abijuru@minervis.com>
2024-07-24 13:31:53 +0200

    Versioning&Changelogs

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 //require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "togo";
-$version = "1.0.20";
+$version = "2.0";
 $ilias_min_version = "5.4.0";
 $ilias_max_version = "8.999";
 $responsible = "Minervis";
